93c5360506ad3414711b8961e87da168744e0dc7
RichardTaku <145557486+RichardTaku@users.noreply.github.com>
2025-03-09 21:07:45 +0530

    Update app-constants.ts

--- src/app-constants.ts
+++ src/app-constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants{
 
-    static BACKEND_API_URL= 'ad6aed2736cdb4a4e94589ffac956d73-1474362618.us-east-1.elb.amazonaws.com';
+    static BACKEND_API_URL= 'http://ad6aed2736cdb4a4e94589ffac956d73-1474362618.us-east-1.elb.amazonaws.com';
 }
